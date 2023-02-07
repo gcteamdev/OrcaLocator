6a22ad311b925a3f5b403c72da8e0b4b92ff456d
GC <contact@gcteam.dev>
2023-02-06 18:55:56 -0800

    google map api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
+import Nav from './Nav';
 import LocationLists from './components/LocationLists';
+import MapContainer from './components/MapContainer';
 
 function App() {
   return (
     <div className="App">
+     
       <div className="header">
         <h1> Orca Locator</h1>
       </div>
-      <LocationLists />
+       <LocationLists />
+       <MapContainer /> 
     </div>
   );
 }
---
+++ src/components/MapContainer.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
+
+const MapContainer = (props) => {
+  const [selectedLocation, setSelectedLocation] = useState({ lat: null, lng: null });
+
+  useEffect((e) => {
+    const geocoder = new window.google.maps.Geocoder();
+    if (window.navigator && window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lng = position.coords.longitude;
+          const latlng = { lat, lng };
+          geocoder.geocode({ location: latlng }, (results, status) => {
+            if (status === "OK") {
+              if (results[0]) {
+                setSelectedLocation({
+                  lat: results[0].geometry.location.lat(),
+                  lng: results[0].geometry.location.lng(),
+                });
+              } else {
+                console.log("No results found");
+              }
+            } else {
+              console.log("Geocoder failed due to: " + status);
+            }
+          });
+        },
+        (error) => console.log(error)
+      );
+    }
+  }, []);
+
+  if (!selectedLocation.lat || !selectedLocation.lng) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <Map
+      google={props.google}
+      zoom={14}
+      center={{
+        lat: selectedLocation.lat,
+        lng: selectedLocation.lng,
+      }}
+    >
+      <Marker
+        position={{
+          lat: selectedLocation.lat,
+          lng: selectedLocation.lng,
+        }}
+        name={"Current Location"}
+      />
+    </Map>
+  );
+};
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyBcKjRkP77W732Q5DFx3n5xJ7kfx6if2Ng",
+})(MapContainer);
